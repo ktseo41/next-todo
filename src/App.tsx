5bc2e020c2fb339f7fb0e01cd8c4f1f3fb32bc23
bohyeon <ktseo41@gmail.com>
2020-11-15 16:28:49 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,84 @@
+import * as React from "react";
+import { v4 } from "uuid";
+import "./styles.scss";
+import defaultTodos from "./todos";
+
+/**
+ * (기본적으로 keep 의 card 들을 상상)
+ * 중요도로 box의 크기를, 급함으로 box의 위치를
+ * 우선순위별로 무조건 처리하지 않는 사람의 불완전함을 수용
+ * 상단에는 equalizer 처럼 ? 보이는 progress bar로 하루를 나타냄
+ * 업무량으로는 색의 농도를 나타낸다.
+ * 다음날로 넘어갈수가 있기 때문에..
+ * 하루의 남은 시간이 예상 업무량만큼에 가깝게 줄어들면 box들이 이동한다.? live switch를 켠다면..
+ *
+ * 제주 카드 내부에는 여러 또다른 카드들이 존재할 수도 있게 재귀구조로
+ * 최대 1단까지만
+ */
+
+/**
+ * 나중에는 월별 Todo, 주별 todo, 일별 todo 등도 나누면 좋을듯
+ * daily todo 일기 쓰기 같은거 보면서 든 생각
+ */
+
+interface INTodoCard {
+  priority: number;
+  urgency: number;
+  weight: number;
+  color?: string;
+  content: any;
+  done: boolean;
+}
+
+const NTodoCard = ({
+  priority,
+  urgency,
+  weight,
+  color,
+  content,
+  done
+}: INTodoCard) => {
+  return (
+    <>
+      <div
+        className={`todo-card priority-${priority} urgency-${urgency} weight-${weight} color-${color} ${
+          done ? "done" : ""
+        }`}
+      >
+        <div
+          className={`todo-card-background weight-${weight} color-${color}`}
+        ></div>
+        {done || <span className="check-todo">TODO</span>}
+        {content}
+      </div>
+    </>
+  );
+};
+
+export default function App() {
+  const [todos, setTodos] = React.useState(defaultTodos);
+  // const [doing, setDoing] = React.useState([]); // 일단 doing은 제외
+  const [dones, setDones] = React.useState([]);
+
+  // console.log(v4().trim());
+
+  return (
+    <div className="App">
+      <h1>Next TODO</h1>
+      <h2>Manage Your Tasks Wisely</h2>
+      <div style={{ margin: "5px 0px", borderBottom: "1px solid black" }}></div>
+      <section className="container">
+        {todos
+          .sort((todoA, todoB) => {
+            if (todoA.done && todoB.done) return todoB.urgency - todoA.urgency;
+            if (todoA.done || todoB.done) return todoA.done ? 1 : -1;
+
+            return todoB.urgency - todoA.urgency;
+          })
+          .map((todo) => {
+            return <NTodoCard {...todo} />;
+          })}
+      </section>
+    </div>
+  );
+}
---
+++ src/todos.ts
@@ -0,0 +1,92 @@
+export default [
+  {
+    key: "009ba715-ca94-453f-95fe-2cac0675c3a7",
+    content: "제주도 옷",
+    priority: 9,
+    urgency: 8,
+    weight: 8,
+    done: false,
+    color: "black"
+  },
+  {
+    key: "e96b5849-542f-4e87-adf0-e1964adc1f4a",
+    content: "에세이",
+    priority: 6,
+    urgency: 4,
+    weight: 9,
+    done: false,
+    color: "black"
+  },
+  {
+    key: "9b098cba-24ce-491f-8483-ffc8db33abd0",
+    content: "크리스마스 숙소 예약",
+    priority: 8,
+    urgency: 10,
+    weight: 8,
+    done: true,
+    color: "black"
+  },
+  {
+    key: "555d4c69-b08f-489e-ade9-4b7cb695b65d",
+    content: "겨울 패딩",
+    priority: 6,
+    urgency: 6,
+    weight: 8,
+    done: false,
+    color: "black"
+  },
+  {
+    key: "6383aa23-fcc0-4881-90cb-a642f194f9de",
+    content: "CJ ONE 포인트 사용",
+    priority: 3,
+    urgency: 3,
+    weight: 5,
+    done: false,
+    color: "black"
+  },
+  {
+    key: "cea2e25f-26f7-4985-97d5-831eae53dd4e",
+    content: "스터디 생각해보기",
+    priority: 5,
+    urgency: 10,
+    weight: 7,
+    done: true,
+    color: "black"
+  },
+  {
+    key: "7b18f4e5-cab8-4277-ba91-697e8c232026",
+    content: "nextjs 블로그 발전시키기",
+    priority: 9,
+    urgency: 4,
+    weight: 9,
+    done: false,
+    color: "black"
+  },
+  {
+    key: "d2cedf62-ec28-429d-8a5b-1698f000e562",
+    content: "독서하기",
+    priority: 10,
+    urgency: 5,
+    weight: 7,
+    done: false,
+    color: "black"
+  },
+  {
+    key: "49274488-977a-4da2-9f30-17b0f788161c",
+    content: "후드티 구매",
+    priority: 3,
+    urgency: 3,
+    weight: 4,
+    done: false,
+    color: "black"
+  },
+  {
+    key: "9868e18d-8ccc-4e21-b48e-c6fef64ffcbd",
+    content: "차트 Wrapper",
+    priority: 8,
+    urgency: 6,
+    weight: 10,
+    done: false,
+    color: "black"
+  }
+];
